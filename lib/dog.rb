c4cab324cab8c5a44facc5c8c463959f9baf82d2
Trisha Garrett <trisha.garrett@gmail.com>
2017-11-23 00:57:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,19 @@
 class Dog
-  def name = (dog_name)
+  def name=(dog_name)
     this_dogs_name = dog_name
   end
 
   def name
     this_dogs_name
   end
-end      
+end     
+
+# class Dog
+#   def name=(dog_name)
+#     this_dogs_name = dog_name
+#   end
+#  
+#   def name
+#     this_dogs_name
+#   end
+# end 
