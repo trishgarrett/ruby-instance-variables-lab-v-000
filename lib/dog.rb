77d359b259c90371de374a87a77a2b78b5407af4
Trisha Garrett <trisha.garrett@gmail.com>
2017-11-23 01:13:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name
+
+# class Dog
+#   def name=(dog_name)
+#     this_dogs_name = dog_name
+#   end
+#
+#   def name
+#     this_dogs_name
+#   end
+# end
